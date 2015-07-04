d4975f71c1b03332f7bf06a7bb4a33a2a8db3176
Steve <stephendwebster@gmail.com>
2015-07-03 22:19:09 -0700

    Make SVG data come from new tuple dataset

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,8 @@
 
 
 d3.select("body").append("p").text("Data visualization");
+
+
 var dataset = [];
 for (var i=0; i<10; i++){
   dataset.push(Math.random() * 100)
@@ -21,7 +23,7 @@ d3.select("body").selectAll("div")
 var dataTuples = [];
 
 for (var i=0; i<10; i++){
-  dataset.push({
+  dataTuples.push({
     pos: i,
     val: Math.random() * 100 
   });
@@ -39,19 +41,39 @@ var svg = d3.select("body")
   .attr("stroke", "orange")
   .attr("stroke-width", 2);
 
-svg.selectAll("rect")
-  .data(dataset)
-  .enter()
-  .append("rect")
-  .attr("x", function(d, i){
-    return (i * svgW / dataset.length);
-  })
-  .attr("y", function(d){
-    console.log("svgH: " + svgH);
-    console.log("d: " + d.val);
-    return svgH - d.val;
-  })
-  .attr("width", svgW / dataset.length - barPadding)
-  .attr("height", function(d){
-    return d.val;
+
+function update(){
+    console.log('updating');
+    console.log(JSON.stringify(dataTuples));
+  svg.selectAll("rect")
+    .data(dataTuples)
+    .enter()
+    .append("rect")
+    .attr("x", function(d){
+      return (d.pos * svgW / dataTuples.length);
     })
+    .attr("y", function(d){
+      // console.log("svgH: " + svgH);
+      // console.log("d: " + d.val);
+       return svgH - d.val;
+    })
+    .attr("width", svgW / dataTuples.length - barPadding)
+    .attr("height", function(d){
+      return d.val;
+    })
+}    
+
+update();
+d3.select("p")
+  .on(
+    "click", function(){
+      alert ("hahaha!");
+    //  var temp = dataTuples[0]["pos"];
+    //  dataTuples[0]["pos"] =  dataTuples[0]["pos"];
+      dataTuples[0]["pos"] = 1;
+      dataTuples[1]["pos"] = 0;
+      console.log('first pos: ', dataTuples[0]["pos"]);    
+      update();
+    })
+
+
