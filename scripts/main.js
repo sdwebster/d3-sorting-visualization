8f3f340a2c4b45269b588f5dc480bbd6c24d307b
Steve <stephendwebster@gmail.com>
2015-07-05 17:18:32 -0700

    Have a simple array underly the array of objects

--- scripts/main.js
+++ scripts/main.js
@@ -1,34 +1,20 @@
-
-
-d3.select("body").append("p").text("Data visualization");
-
+d3.select("body").append("p").text("Visualizing Sorting Algorithms");
 
 var dataset = [];
-for (var i=0; i<10; i++){
-  dataset.push(Math.random() * 100)
-}
 
-d3.select("body").selectAll("div")
-  .data(dataset)
-  .enter()
-  .append('div')
-  .attr('class', 'bar')
-  .style('height', function(d){
-    return d + 'px';
-  })
-  .style('opacity', function(d){
-      return d / 100;
-  });
+for (var i=0; i<10; i++){
+  dataset.push(
+    Math.random() * 100 
+  );
+}
 
 var dataTuples = [];
-
-for (var i=1; i<10; i++){
+for (var i=0; i<10; i++){
   dataTuples.push({
     pos: i,
-    val: Math.random() * 100 
+    val: dataset[i]
   });
 }
-
 
 console.log(JSON.stringify(dataTuples));
 
@@ -36,14 +22,27 @@ var svgW = 800;
 var svgH = 200;
 var barPadding = 5;
 var stroke = 3;
+var border = 2;
+var bordercolor = "black";
 
 var svg = d3.select("body")
   .append("svg")
   .attr("width", svgW)
   .attr("height", svgH)
   .attr("stroke", "orange")
-  .attr("stroke-width", 2);
+  .attr("stroke-width", 2)
+  .attr("border", border);
 
+// var borderPath = svg.append('div').append('rect')
+//   .attr("x", 0)
+//   .attr("y", 0)
+//   .attr("width",svgW)
+//   .attr("height",svgH)
+//   .attr("stroke", bordercolor)
+//   .attr("fill","none")
+//   .attr("stroke-width", border);
+// 
+// var bars = svg.append("div");
 
 function update(){
     console.log('updating');
@@ -53,65 +52,52 @@ function update(){
     .enter()
     .append("rect")
     .attr("x", function(d, i){
-      console.log('for pos ', d.pos, 'and index ', i, ', x is ', (d.pos * svgW / dataTuples.length));
-      return (d.pos * svgW / dataTuples.length);
+    //  console.log('for pos ', d.pos, 'and index ', i, ', x is ', (d.pos * svgW / dataTuples.length));
+      return ((d.pos * svgW + 1)  / dataTuples.length);
     })
     .attr("y", function(d){
-      // console.log("svgH: " + svgH);
-      // console.log("d: " + d.val);
+       console.log("svgH: " + svgH);
+       console.log("d: " + d.val);
        return svgH - d.val;
     })
     .attr("width", svgW / dataTuples.length - barPadding)
     .attr("height", function(d){
       return d.val * 2;
-    })
-
-//  svg.selectAll("rect")
-//    .data("dataTuples")
-//    .transition()
-//    .duration(1000) // 
-//    .attr("x", function(d){
-//      return (d.pos * svgW / dataTuples.length);
-//    })
-//    .attr("y", function(d){
-//      // console.log("svgH: " + svgH);
-//      // console.log("d: " + d.val);
-//       return svgH - d.val;
-//    })
-//    .attr("width", svgW / dataTuples.length - barPadding)
-//    .attr("height", function(d){
-//      return d.val;
-//    })
-}    
+    });
+}
 
 update();
+
+// function findIndexFromPos(data, position){
+
+//}
+
 d3.select("p")
   .on(
     "click", function(){
-      alert ("hahaha!");
     //  var temp = dataTuples[0]["pos"];
     //  dataTuples[0]["pos"] =  dataTuples[0]["pos"];
-      dataTuples[0]["pos"] = 2;
-      dataTuples[1]["pos"] = 1;
-      console.log('first pos: ', dataTuples[0]["pos"]);
-      // update();
-    svg.selectAll("rect")
-      .data(dataTuples)
-      .transition()
-      .duration(1000) // 
-      .attr("x", function(d, i){
-      console.log('for pos ', d.pos, 'and index ', i, ', x is ', (d.pos * svgW / dataTuples.length));
-        return ((d.pos * svgW + 1) / dataTuples.length);
-      })
-      .attr("y", function(d){
-        // console.log("svgH: " + svgH);
-        // console.log("d: " + d.val);
-         return svgH - d.val;
-      })
-      .attr("width", svgW / dataTuples.length - barPadding)
-      .attr("height", function(d){
-        return d.val;
-      })
+
+      loI = 0;
+      hiI = 1;
+      var temp = dataTuples[loI]["pos"];
+      dataTuples[loI]["pos"] = hiI;
+      dataTuples[hiI]["pos"] = temp;
+      svg.selectAll("rect")
+        .data(dataTuples)
+        .transition()
+        .duration(1000) // 
+        .attr("x", function(d, i){
+        console.log('for pos ', d.pos, 'and index ', i, ', x is ', (d.pos * svgW / dataTuples.length));
+          return ((d.pos * svgW + 1) / dataTuples.length);
+        })
+        .attr("y", function(d){
+          // console.log("svgH: " + svgH);
+          // console.log("d: " + d.val);
+           return svgH - d.val;
+        })
+        .attr("width", svgW / dataTuples.length - barPadding)
+        .attr("height", function(d){
+          return d.val;
+        })
     })
-
-
